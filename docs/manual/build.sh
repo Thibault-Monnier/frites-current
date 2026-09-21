dceb767fd7196fb4e795a859eba51d787d290706
Thibault-Monnier <thibaultmonni@gmail.com>
2026-09-21 22:04:17 +0200

    Delete outdated `docs/manual`

--- docs/manual/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd sources
-pdflatex -interaction=nonstopmode -shell-escape main.tex
-pdflatex -interaction=nonstopmode -shell-escape main.tex
-mv main.pdf ../manual.pdf
-rm *.aux *.log *.toc *.out
-cd ..
-echo "PDF manual generated at ./manual.pdf"
